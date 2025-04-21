fc53ab65203fb0e3960868f5e3a3e08ae9b75bfb
umer <mumarkhan383@gmail.com>
2025-04-21 16:18:20 +0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  
+  return (
+    <div className=''>
+      <h1 className="font-extralight text-center text-red-500">
+        Hello world!
+      </h1>
+    </div>
+  )
+}
+
+export default App
